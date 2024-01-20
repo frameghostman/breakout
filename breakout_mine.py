d4291ddaa8875183f59f071db8a80d75a5de0a3f
frameghostman <devil_orebaka@hotmail.com>
2024-01-20 21:45:31 +0900

    commit

--- breakout_mine.py
+++ breakout_mine.py
@@ -15,31 +15,50 @@ BLUE = (0, 0, 255)
 # ゲームウィンドウの作成
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-# パドルの設定
+# パドルサイズ
 PADDLE_WIDTH, PADDLE_HEIGHT = 200, 10
+
+# パドル位置
 paddle_x = (WIDTH - PADDLE_WIDTH) // 2
 paddle_y = HEIGHT - 50
 
-# ボールの設定
+# ボールサイズ
 BALL_RADIUS = 20
-ball_x = (WIDTH - BALL_RADIUS) // 2
-ball_y = HEIGHT - 80
 ball_speed_x = 5
 ball_speed_y = 5
 
-# ブロックの設定
+# ボール位置
+BALL_RADIUS = 20
+ball_x = (WIDTH - BALL_RADIUS) // 2
+ball_y = HEIGHT - 80
+
+# ブロック設定
 BLOCK_WIDTH, BLOCK_HEIGHT = 50, 20
 block_rows = 4
 block_cols = 12
 blocks = []
 
-for row in range(block_rows):
-    for col in range(block_cols):
-        block = pygame.Rect(col * BLOCK_WIDTH, row * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT)
-        blocks.append(block)
-
 # ゲーム状態
 page = "title"
+
+# initilize
+def gameinit():
+    global paddle_x, paddle_y, ball_x, ball_y
+    # ボール位置
+    BALL_RADIUS = 20
+    ball_x = (WIDTH - BALL_RADIUS) // 2
+    ball_y = HEIGHT - 80
+
+    # パドル位置
+    paddle_x = (WIDTH - PADDLE_WIDTH) // 2
+    paddle_y = HEIGHT - 50
+
+    # ブロック配列
+    blocks.clear()
+    for row in range(block_rows):
+        for col in range(block_cols):
+            block = pygame.Rect(col * BLOCK_WIDTH, row * BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT)
+            blocks.append(block)
 
 # title
 def title():
@@ -99,7 +118,7 @@ def gamestage():
 
 # game over
 def gameover():
-    global page, ball_x, ball_y
+    global page
     screen.fill(pygame.Color("NAVY"))
     font = pygame.font.Font(None, 100)
     text = font.render("GAME OVER", True, pygame.Color("RED"))
@@ -109,11 +128,11 @@ def gameover():
     screen.blit(text, (100,200))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
-        ball_x = (WIDTH - BALL_RADIUS) // 2
-        ball_y = HEIGHT - 80
+        gameinit()
         page = "main"
 
 # ゲームループ
+gameinit()
 while True:
     # フレームレート設定
     pygame.time.Clock().tick(60)
